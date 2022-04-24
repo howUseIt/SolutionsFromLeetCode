91ad51d6c6107d6c5eb0f0652b872d5d32c12944
howUseIt <microzzoft@gmail.com>
2022-04-24 12:48:51 +0500

    Reverse String

---
+++ TopInterviewQuestions/EasyCollection/String/reverseString.kt
@@ -0,0 +1,11 @@
+fun reverseString(s: CharArray): Unit {
+    var i = 0
+    var j = s.size - 1
+    while (i <= j) {
+        val temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+        i++
+        j--
+    }
+}
